5163942b0f95a8b7b79e188092a6b4f99004acff
Li Guang <liguangbz@sina.com>
2016-06-04 21:32:44 +0800

    add main page
    Signed-off-by: Li Guang <liguangbz@sina.com>

--- tbrain.js
+++ tbrain.js
@@ -38,7 +38,7 @@ layer_defs.push({type:'fc', num_neurons:200, activation:'relu'});
 //layer_defs.push({type:'pool', sx:2, stride:2});
 //layer_defs.push({type:'conv', sx:5, filters:16, stride:1, pad:2, activation:'relu'});
 //layer_defs.push({type:'pool', sx:3, stride:3});
-layer_defs.push({type:'softmax', num_classes:124});
+layer_defs.push({type:'softmax', num_classes:127});
 
 function query_labels(fpool)
 {
